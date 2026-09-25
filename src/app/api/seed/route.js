26f42c3a6c01e92f3f5546db775832f5f1a8321f
AKASH <v.akash5555@gmail.com>
2026-09-25 20:43:42 +0530

    Updated croissant image link

--- src/app/api/seed/route.js
+++ src/app/api/seed/route.js
@@ -65,7 +65,7 @@ export async function GET() {
             },
             {
                 name: "Chocolate Croissant", category: "Food", basePrice: 180, isAvailable: true,
-                imageURL: "https://images.unsplash.com/photo-1549903072-7e6e0fb26fff?w=800&q=80",
+                imageURL: "https://images.unsplash.com/photo-1649019937955-9687640aaaab?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 description: "Buttery, flaky pastry filled with rich dark chocolate.",
                 availableOutlets: ALL_OUTLETS,
                 customisations: [{ type: "Warming", options: [{ name: "Normal", priceDelta: 0 }, { name: "Warm", priceDelta: 0 }] }]
